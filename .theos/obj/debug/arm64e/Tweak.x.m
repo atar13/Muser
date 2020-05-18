ecd80b7980a9702cfee9484fc7f3d17d770bbb16
Anthony Tarbinian <42757207+atar13@users.noreply.github.com>
2020-05-18 15:26:31 -0700

    font kinda works

--- .theos/obj/debug/arm64e/Tweak.x.m
+++ .theos/obj/debug/arm64e/Tweak.x.m
@@ -5,6 +5,9 @@
 @end
 
 
+NSString *labelText = @"label";
+
+
 #include <substrate.h>
 #if defined(__clang__)
 #if __has_feature(objc_arc)
@@ -25,24 +28,51 @@
 #define _LOGOS_RETURN_RETAINED
 #endif
 
-@class UINavigationBarLargeTitleView; 
-static void (*_logos_orig$_ungrouped$UINavigationBarLargeTitleView$setTitle$)(_LOGOS_SELF_TYPE_NORMAL id _LOGOS_SELF_CONST, SEL, id); static void _logos_method$_ungrouped$UINavigationBarLargeTitleView$setTitle$(_LOGOS_SELF_TYPE_NORMAL id _LOGOS_SELF_CONST, SEL, id); 
+@class UINavigationBarLargeTitleView; @class UITableViewLabel; 
+static void (*_logos_orig$_ungrouped$UINavigationBarLargeTitleView$setTitle$)(_LOGOS_SELF_TYPE_NORMAL id _LOGOS_SELF_CONST, SEL, id); static void _logos_method$_ungrouped$UINavigationBarLargeTitleView$setTitle$(_LOGOS_SELF_TYPE_NORMAL id _LOGOS_SELF_CONST, SEL, id); static void (*_logos_orig$_ungrouped$UITableViewLabel$setFont$)(_LOGOS_SELF_TYPE_NORMAL id _LOGOS_SELF_CONST, SEL, id); static void _logos_method$_ungrouped$UITableViewLabel$setFont$(_LOGOS_SELF_TYPE_NORMAL id _LOGOS_SELF_CONST, SEL, id); 
 
-#line 6 "Tweak.x"
+#line 9 "Tweak.x"
 
 
 	static void _logos_method$_ungrouped$UINavigationBarLargeTitleView$setTitle$(_LOGOS_SELF_TYPE_NORMAL id _LOGOS_SELF_CONST __unused self, SEL __unused _cmd, id arg1){
 	_logos_orig$_ungrouped$UINavigationBarLargeTitleView$setTitle$(self, _cmd, arg1);
 	
 	UINavigationBarLargeTitleView *swift = self;
+	
 	if([swift.title isEqualToString:@"Library"]){
-	 swift.title = @"bruh";
+			swift.title = labelText;
+		
 	}
 	}
 	
 
 
 
-static __attribute__((constructor)) void _logosLocalCtor_e641bec3(int __unused argc, char __unused **argv, char __unused **envp) {
-	{Class _logos_class$_ungrouped$UINavigationBarLargeTitleView = NSClassFromString(@"_UINavigationBarLargeTitleView"); MSHookMessageEx(_logos_class$_ungrouped$UINavigationBarLargeTitleView, @selector(setTitle:), (IMP)&_logos_method$_ungrouped$UINavigationBarLargeTitleView$setTitle$, (IMP*)&_logos_orig$_ungrouped$UINavigationBarLargeTitleView$setTitle$);}
+@interface UITableViewLabel : UILabel
+@property (nonatomic, strong, readwrite) UIFont *font;
+@end
+
+
+	
+
+	static void _logos_method$_ungrouped$UITableViewLabel$setFont$(_LOGOS_SELF_TYPE_NORMAL id _LOGOS_SELF_CONST __unused self, SEL __unused _cmd, id arg2){
+			UITableViewLabel *tableViewSwift = self;
+			UIFont *defaultFont = tableViewSwift.font;
+		_logos_orig$_ungrouped$UITableViewLabel$setFont$(self, _cmd, arg2);
+
+
+
+		if([tableViewSwift.font isEqual:defaultFont]){
+			
+					labelText = tableViewSwift.font.fontName;
+
+		}
+		
+
+	}
+
+
+static __attribute__((constructor)) void _logosLocalCtor_de956a09(int __unused argc, char __unused **argv, char __unused **envp) {
+	{Class _logos_class$_ungrouped$UINavigationBarLargeTitleView = NSClassFromString(@"_UINavigationBarLargeTitleView"); MSHookMessageEx(_logos_class$_ungrouped$UINavigationBarLargeTitleView, @selector(setTitle:), (IMP)&_logos_method$_ungrouped$UINavigationBarLargeTitleView$setTitle$, (IMP*)&_logos_orig$_ungrouped$UINavigationBarLargeTitleView$setTitle$);Class _logos_class$_ungrouped$UITableViewLabel = NSClassFromString(@"UITableViewLabel"); MSHookMessageEx(_logos_class$_ungrouped$UITableViewLabel, @selector(setFont:), (IMP)&_logos_method$_ungrouped$UITableViewLabel$setFont$, (IMP*)&_logos_orig$_ungrouped$UITableViewLabel$setFont$);}
+	
 	}
--- Tweak.x
+++ Tweak.x
@@ -3,20 +3,50 @@
 @property (nonatomic, copy, readwrite) NSString *title;
 @end
 
+
+NSString *labelText = @"label";
+
 %hook UINavigationBarLargeTitleView
 
 	-(void)setTitle:(id)arg1{
 	%orig;
 	
 	UINavigationBarLargeTitleView *swift = self;
+	// NSLOG(@"TEXT");
 	if([swift.title isEqualToString:@"Library"]){
-	 swift.title = @"bruh";
+			swift.title = labelText;
+		
 	}
 	}
 	
 
 %end
 
+@interface UITableViewLabel : UILabel
+@property (nonatomic, strong, readwrite) UIFont *font;
+@end
+
+%hook UITableViewLabel
+	
+
+	-(void)setFont:(id)arg2{
+			UITableViewLabel *tableViewSwift = self;
+			UIFont *defaultFont = tableViewSwift.font;
+		%orig;
+
+
+
+		if([tableViewSwift.font isEqual:defaultFont]){
+			// tableViewSwift.font = [tableViewSwift.font fontWithSize:(CGFloat)12];
+					labelText = tableViewSwift.font.fontName;
+
+		}
+		
+
+	}
+%end
+
 %ctor {
-	%init(_ungrouped, UINavigationBarLargeTitleView = NSClassFromString(@"_UINavigationBarLargeTitleView"));
+	%init(_ungrouped, UINavigationBarLargeTitleView = NSClassFromString(@"_UINavigationBarLargeTitleView"), UITableViewLabel = NSClassFromString(@"UITableViewLabel"));
+	
 	}
